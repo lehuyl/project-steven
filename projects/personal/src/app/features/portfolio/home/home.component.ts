beb6eb1d10008be878de481a570c4eb3f93ee1c1
streamlineSteven <steven@streamlineathletes.com>
2020-04-05 14:43:37 -0700

    Added Skills and Project section // fixed icons
    Signed-off-by: streamlineSteven <steven@streamlineathletes.com>

--- projects/personal/src/app/features/portfolio/home/home.component.ts
+++ projects/personal/src/app/features/portfolio/home/home.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lehuyl-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'lehuyl-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+    covidDescription: String;
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        this.covidDescription = 'Used Amcharts to create a time series graph that depict worldwide infection and death rates. ';
+        this.covidDescription += 'We fetched data from one of many endpoints and stored them in state using NGRX.'
+    }
 }
